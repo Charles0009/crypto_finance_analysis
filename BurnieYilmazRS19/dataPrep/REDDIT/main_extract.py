c667073130c80b6a0f6d080d0a13e5b49246e169
Charles0009 <charles.laverdure02@gmail.com>
2022-02-27 21:43:13 +0100

    work on the automatisation of the reddit price plotting

--- BurnieYilmazRS19/dataPrep/REDDIT/main_extract.py
+++ BurnieYilmazRS19/dataPrep/REDDIT/main_extract.py
@@ -6,43 +6,47 @@ import os
 import datetime
 
 
-start = 1634251200
-end =   1644830527
-subreddit= 'CryptoMarkets'
-
-start_in_date = datetime.datetime.fromtimestamp(start)
-end_in_date = datetime.datetime.fromtimestamp(end)
-print(start_in_date)
-print(end_in_date)
+# start = 1634251200
+# end =   1644830527
+# subreddit= 'CryptoMarkets'
 
 
-start_in_date = str(start_in_date)
-end_in_date = str(end_in_date)
-start_in_date = start_in_date[:10]
-end_in_date = end_in_date[:10]
-
-current_directory = str(os.getcwd())
-
-name_for_file = subreddit+"_"+start_in_date+"_"+end_in_date
-name_for_file_extracting = current_directory+'/BurnieYilmazRS19/dataPrep/REDDIT/data/extracting/'+name_for_file+'.pkl'
-
-print(name_for_file_extracting)
-
-name_for_saving_processed = current_directory+'/BurnieYilmazRS19/dataPrep/REDDIT/data/processing/tokenFreq/'+name_for_file+'.pkl'
+def extract_full_reddit_token_frequency(start_epoch, end_epoch, subreddit):
+    start_in_date = datetime.datetime.fromtimestamp(start_epoch)
+    end_in_date = datetime.datetime.fromtimestamp(end_epoch)
+    print(" start date : " + start_in_date)
+    print(" start date : " + end_in_date)
 
 
-#call to the api for extraction
-print("data extracting")
-data_extracting( start=start, 
-                 end=end ,
-                 subreddit=subreddit,
-                 name_for_file_extracting = name_for_file_extracting )
-#call to the api for extraction
-print("text processing")
-textprocess(name_for_file_extracting)
-print("daily word frequency")
-dailywordfreq( start=start, 
-                end=end, 
-                name_for_saving_processed = name_for_saving_processed   )
+    start_in_date = str(start_in_date)
+    end_in_date = str(end_in_date)
+    start_in_date = start_in_date[:10]
+    end_in_date = end_in_date[:10]
 
-print(name_for_saving_processed)
+    current_directory = str(os.getcwd())
+
+    name_for_file = subreddit+"_"+start_in_date+"_"+end_in_date
+    name_for_file_extracting = current_directory+'/BurnieYilmazRS19/dataPrep/REDDIT/data/extracting/'+name_for_file+'.pkl'
+
+    print("name for file extracting : " + name_for_file_extracting)
+
+    name_for_saving_processed = current_directory+'/BurnieYilmazRS19/dataPrep/REDDIT/data/processing/tokenFreq/'+name_for_file+'.pkl'
+
+
+    #call to the api for extraction
+    print("data extracting")
+    data_extracting( start=start_epoch, 
+                    end=end_epoch ,
+                    subreddit=subreddit,
+                    name_for_file_extracting = name_for_file_extracting )
+    #call to the api for extraction
+    print("text processing")
+    textprocess(name_for_file_extracting)
+    print("daily word frequency")
+    dailywordfreq( start=start_epoch, 
+                    end=end_epoch, 
+                    name_for_saving_processed = name_for_saving_processed   )
+
+    print("name for savec file : " + name_for_saving_processed)
+
+    return(name_for_saving_processed)
--- test2.py
+++ test2.py
@@ -1,12 +1,30 @@
 
+import os
 import matplotlib.pyplot as plt
 from turtle import left
 from apis.get_data_api_b import get_list_of_coins, get_list_of_crypto_names, get_pd_daily_histo_between_dates
+from BurnieYilmazRS19.dataPrep.REDDIT.main_extract import extract_full_reddit_token_frequency
 
 import pandas as pd
 
+#get token frequency 
 
-reddit = pd.read_pickle('BurnieYilmazRS19/dataPrep/REDDIT/data/processing/tokenFreq/CryptoMarkets_2021-10-15_2022-02-14.pkl')
+
+# start = 1634251200
+# end =   1644830527
+# subreddit= 'CryptoMarkets'
+
+
+name_for_file_pickle_reddit = extract_full_reddit_token_frequency(1634251200,1644830527, 'CryptoMarkets')
+
+
+
+current_directory = str(os.getcwd())
+path_for_pickle_file = current_directory+'/BurnieYilmazRS19/dataPrep/REDDIT/data/extracting/'+name_for_file_pickle_reddit+'.pkl'
+
+reddit = pd.read_pickle(path_for_pickle_file)
+
+
 
 col_list_names = get_list_of_crypto_names()
 col_list_symbol = get_list_of_coins()
@@ -50,5 +68,18 @@ final_df.columns = ['Open_Time', 'nb_mentions']
 pd_eth = get_pd_daily_histo_between_dates('ETHUSDT', '15 Oct 2021', '14 Feb 2022')
 
 combined_df = pd.merge(final_df, pd_eth, on=['Open_Time'])
+
+
+# plt.subplot(2, 1, 1)
+plt.plot(combined_df['Open_Time'], combined_df['Close'], color='blue')
+plt.ylabel('price')
+plt.twinx()
+plt.bar(combined_df['Open_Time'], combined_df['nb_mentions'],color='green')
+plt.ylabel('nb mentions reddit')
+
+
+# plt.plot(combined_df['Open_Time'], combined_df['Close'])
+# plt.bar(combined_df['Open_Time'],combined_df['Nb_mentions'])
+plt.show()
 
 
